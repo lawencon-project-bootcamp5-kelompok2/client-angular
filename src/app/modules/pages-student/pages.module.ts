e8f906aa6dea2d67cb6b14cdfbae3e4f57c325ff
rizkymf <rizky.mochamadf@gmail.com>
2020-04-15 11:58:37 +0700

    tambah timer, forum ada di detail-materi, page forum masih ada buat jaga2

--- src/app/modules/pages-student/pages.module.ts
+++ src/app/modules/pages-student/pages.module.ts
@@ -5,10 +5,15 @@ import {PagesRoutingModule} from './pages-routing.module';
 import {PagesComponent} from './student/pages.component';
 import {DashBoardComponent} from './dash-board/dash-board.component';
 import {LayoutModule} from '../layout/layout.module';
-
+import { EnrollComponent } from './enroll/enroll.component';
+import { DetailMateriComponent } from './detail-materi/detail-materi.component';
+import { PembelajaranComponent } from './list-pembelajaran/pembelajaran.component';
+import { NilaiComponent } from './nilai/nilai.component';
+import { TestPageComponent } from './test-page/test-page.component';
 
 @NgModule({
-  declarations: [PagesComponent, DashBoardComponent],
+  declarations: [PagesComponent, DashBoardComponent, EnrollComponent, DetailMateriComponent, PembelajaranComponent,
+  NilaiComponent, TestPageComponent],
   imports: [
     CommonModule,
     PagesRoutingModule,
--- src/app/modules/pages-student/test-page/test-page.component.ts
+++ src/app/modules/pages-student/test-page/test-page.component.ts
@@ -6,11 +6,35 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./test-page.component.scss']
 })
 export class TestPageComponent implements OnInit {
+  // TODO: kondisi buttonOn ngeGet dari backend
+  buttonOn = true;
 
   constructor() {
   }
 
   ngOnInit() {
+    setInterval(this.fungsi, 1000);
   }
 
+  fungsi() {
+
+    let countDeadline = new Date("Apr 15, 2020 15:00:00").getTime();
+
+    let now = new Date().getTime();
+  
+    let distance = countDeadline - now;
+  
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+    if (distance < 0) {
+      document.getElementById("infoTest").innerHTML = "<font color='red'><b>Maaf anda tidak bisa upload karena waktu sudah habis<b></font>";
+      document.getElementById("timer").innerHTML ="00:00:00";
+    } else {
+      document.getElementById("infoTest").innerHTML = "<b>Upload jawaban dibawah ini sebelum waktu habis<b>";
+      document.getElementById("timer").innerHTML = hours.toString().padStart(2, "0") + ":" + 
+        minutes.toString().padStart(2, "0") + ":" + seconds.toString().padStart(2, "0");
+    }
+  }
 }
